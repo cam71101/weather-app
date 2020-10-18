4765edf8a5e52f74e462e1b1de1b5b04b445fcf8
cam71101 <david.fisher137@gmail.com>
2020-10-18 16:27:55 +0100

    Updates

---
+++ precache-manifest.5a0184aa53a92ec7c82912e358bbc051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818474266128c1f54753066739946bd8",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8c2faa497284d6a41bba",
+    "url": "/weather-app/static/css/main.22ee5dcb.chunk.css"
+  },
+  {
+    "revision": "fa0e211e647b771f999b",
+    "url": "/weather-app/static/js/2.acb63ba8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.acb63ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2faa497284d6a41bba",
+    "url": "/weather-app/static/js/main.d9ab47ad.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "f9737d3ed2e6a8b07c15ff443e9d173d",
+    "url": "/weather-app/static/media/magnifying-glass.f9737d3e.svg"
+  }
+]);
